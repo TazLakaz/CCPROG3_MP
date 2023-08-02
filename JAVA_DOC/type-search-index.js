880b13059d96f3fa6d5faf9d514239b18dbb9d03
TazLakaz <141283224+TazLakaz@users.noreply.github.com>
2023-08-03 07:01:23 +0800

    Add files via upload

---
+++ JAVA_DOC/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Item"},{"p":"<Unnamed>","l":"RegularVM"},{"p":"<Unnamed>","l":"Slots"},{"p":"<Unnamed>","l":"SpecialVM"},{"p":"<Unnamed>","l":"TransactionHistory"},{"p":"<Unnamed>","l":"VendingMachine"}];updateSearchResults();
